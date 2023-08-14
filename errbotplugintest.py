fcf36a78e2f054058381dfc89586aa7f33fff881
Валерий В. Исаков <user.email=isakovvv@yoomoney.ru>
2023-08-14 22:18:19 +0300

    fix

--- errbotplugintest.py
+++ errbotplugintest.py
@@ -55,7 +55,8 @@ class errbotplugintest(BotPlugin):
 
 
         #try:
-        return 'test: {0}'.format(message.frm)
+        #return 'test: {0}'.format(message.frm)
+        return 'test'
         #except Exception as e:
         #   self.log.exception(e)
         #    yield "Request processing error. See errbot logs for details"
