51a44f963edbe9f1574c86d68e6d4e1a04ba8898
Валерий В. Исаков <user.email=isakovvv@yoomoney.ru>
2023-08-22 22:48:05 +0300

    fix

--- errbotplugintest.py
+++ errbotplugintest.py
@@ -74,7 +74,7 @@ class errbotplugintest(BotPlugin):
         staging_pattern = 'stg|staging|pre-production'
 
         if re.match(staging_pattern, env):
-            errbotplugintest.errbotplugintest_deploy(self, message, env)
+            next(errbotplugintest.errbotplugintest_deploy(self, message, env))
         else:
             raise ValidationException(
                 "You can deploy only on staging environments"
