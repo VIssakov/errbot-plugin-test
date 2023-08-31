10d31ea2c45e783b6fd68be096ca94d0b313c909
Валерий В. Исаков <user.email=isakovvv@yoomoney.ru>
2023-08-31 18:24:33 +0300

    fix

--- errbotplugintest.py
+++ errbotplugintest.py
@@ -75,8 +75,8 @@ class errbotplugintest(BotPlugin):
         staging_pattern = 'stg|staging|pre-production'
 
         print(server, branch)
-        message = message.replace('dev ', '')
-        print(message)
+        #message = message.replace('dev ', '')
+        print(type(message))
 
         if re.findall(staging_pattern, server):
             yield next(self.errbotplugintest_deploy(message, server, branch))
